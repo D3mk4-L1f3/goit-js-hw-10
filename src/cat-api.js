c5a22c68df891da33a816c4c653753ae72375539
D3mk4 <dmtvchenko@gmail.com>
2023-08-21 10:20:07 +0300

    theSecondFixing

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
-const API_KEY = "live_L0jvsXCus7UsmimSLLHveKMvHoGwXBTyJ06eJCJjQsFwCgHb9nRp2hbLd6KkimC9";
-axios.defaults.headers.common["x-api-key"] = API_KEY;
+axios.defaults.headers.common["x-api-key"] =
+  "live_L0jvsXCus7UsmimSLLHveKMvHoGwXBTyJ06eJCJjQsFwCgHb9nRp2hbLd6KkimC9";
 const CAT_URL = "https://api.thecatapi.com/v1";
 
 export function fetchBreeds() {
   return axios.get(`${CAT_URL}/breeds`)
     .then(response => response.data)
-      .catch(error => {
-          throw error;
+    .catch(error => {
+      throw error;
     });
 }
 export function fetchCatByBreed(breedId) {
   return axios.get(`${CAT_URL}/images/search?breed_ids=${breedId}`)
     .then(response => response.data[0])
-      .catch(error => {
-          throw error;
+    .catch(error => {
+      throw error;
     });
 }
+
--- src/index.js
+++ src/index.js
@@ -5,39 +5,42 @@ const catDescription = document.querySelector('.cat-info');
 const loadString = document.querySelector('.loader');
 const errorString = document.querySelector('.error');
 
-function catsBreedDescription(data) {
-    const { url, width, height, breeds } = data;
-    const { name, description, temperament } = breeds[0];
-    
-  const createCatCard = `
-    <img src="${url}" width="${width}" height="${height}" />
+function displayCatBreedInfo(data) {
+  const { url, breeds } = data;
+  const { name, description, temperament } = breeds[0];
+
+  const catCardHTML = `
+    <img src="${url}" alt="${name}" />
     <h2>${name}</h2>
     <p>${description}</p>
     <p>${temperament}</p>
   `;
-  catDescription.insertAdjacentHTML('beforeend', createCatCard);
+  catDescription.innerHTML = catCardHTML;
 
   loadString.setAttribute('hidden', '');
   catDescription.removeAttribute('hidden');
 }
+
 function onError() {
   errorString.removeAttribute('hidden');
   loadString.setAttribute('hidden', '');
   catDescription.setAttribute('hidden', '');
+  selectForm.setAttribute('hidden', '');
 }
-if (selectForm && loadString) {
+
+if (selectForm) {
   loadString.removeAttribute('hidden');
   errorString.setAttribute('hidden', '');
 
   fetchBreeds()
     .then(breeds => {
-        const interfaceOptions = breeds.map(breed => `
-    <option value="${breed.id}">${breed.name}</option>`)
-    .join('');
+      const interfaceOptions = breeds
+        .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+        .join('');
+      selectForm.innerHTML = interfaceOptions;
 
-        selectForm.insertAdjacentHTML('beforeend', interfaceOptions);
-        selectForm.removeAttribute('hidden');
-        loadString.setAttribute('hidden', '');
+      selectForm.removeAttribute('hidden');
+      loadString.setAttribute('hidden', '');
 
       selectForm.addEventListener('change', evt => {
         const breedId = evt.target.value;
@@ -47,24 +50,19 @@ if (selectForm && loadString) {
 
         fetchCatByBreed(breedId)
           .then(catData => {
-            catsBreedDescription(catData);
+            displayCatBreedInfo(catData);
+            loadString.setAttribute('hidden', '');
+            catDescription.removeAttribute('hidden');
           })
           .catch(error => {
             onError();
           });
-
-        loadString.removeAttribute('hidden');
-        catDescription.setAttribute('hidden', '');
-        errorString.setAttribute('hidden', '');
       });
     })
     .catch(error => {
       onError();
     });
 }
-
-
-
 
 
 
